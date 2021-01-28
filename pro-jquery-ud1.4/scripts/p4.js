13a6d07757add46475392cc40e5a60e8153e83d6
miralles_660 <miralles902@gmail.com>
2021-01-28 02:36:42 +0100

    nuevos comentarios practica 4

--- pro-jquery-ud1.4/scripts/p4.js
+++ pro-jquery-ud1.4/scripts/p4.js
@@ -9,8 +9,9 @@ $('p:not(div#footer p)').after($top);
 //punt 2 $('span.footnote').insertBefore('#footer')
 //punt 3 $('span.footnote').wrapAll("<ol></ol>").wrap('<li></li>');
 
+//punt 4
 $('span.footnote').insertBefore('#footer').wrapAll("<ol></ol>").wrap('<li></li>');
 
-//punt 1 refet amb insertAfter y prependTo
+//punt 5 (punt 1 refet amb insertAfter y prependTo)
 $('<a href="#">Back To Top</a>').prependTo('body').insertAfter('p:not(div#footer p)');
 });
