0b9d2a9a46f38234eba64344bd2de0690e93258e
miralles_660 <miralles902@gmail.com>
2021-02-04 21:53:50 +0100

    ejercicio 1 ud 2 Jquery

--- scripts/UD2.p1.js
+++ scripts/UD2.p1.js
@@ -1,14 +1,25 @@
-$(function() {
-$('body').find('p').on('click',function(evento){
-    evento.preventDefault();
-    let separadorEsp = (' ');
-    let separadorPunt = ('.');
-var textos = $('body').find('p');
-var separarTexto = textos.split(separadorEsp);
-var separarTexto2 = textos.split(separadorPunt);
+$(function () {
+          let span = "<span>"; //creamos etiqueta span
+          let $sizePalabra = $('p').html().length; //comprobamos la longitud de dentro de p
+          let $texto = $('p').html(); //conseguimos el texto
+          let vacia = ""; //cadena vacia, a la que insertaremos el contenido
 
-$(this).css('background-color','yellow','font-weight','bold');
-$(this).css('font-weight','bold');
-});
+          for(let i = 0; i < $sizePalabra; i++){ //recorremos
 
+              span += $texto[i]; //añadimos el primer <span>
+
+              if($texto[i] == " "){
+                  span += "</span>"; //cerramos la palabra 
+                  vacia += span; //asignamos el texto con <span> a la cadena vacia
+                  span = "<span>"; //introducimos para insertar la siguiente palabra
+              }
+          }
+          $('p').html(vacia); //introducimos en p la nueva cadena
+
+    $("p").find("span").on("click", function (evento) {
+      //al clickar sobre el elemento
+      evento.preventDefault(); //evitamos acciones indeseadas
+      //hacemos que la palabra (this) tengo el fondo amarillo y este en negrita
+      $(this).css({"background-color": "yellow","font-weight": "bold"}); //css multiple  
+    });
 });
--- scripts/UD2.p2.js
+++ scripts/UD2.p2.js
@@ -1,6 +1,16 @@
 $(function() {
+let $switcher = $('.switcher');
+let $botones = $('.switcher').children('button').data('style');
 
+//$botones.remove();
 
+$switcher.hover(function(){
+//entro
+$(this).addClass('hover');
+}, function(){
+//salgo
+$(this).removeClass();
+$(this).addClass('switcher');
+}); 
 
-    
 });
